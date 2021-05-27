5dd84e411a95840c69b662fc1a59a48b7084a517
Nicu <vasilenicolae.101@gmail.com>
2021-05-27 21:27:50 +0300

    initialized Footer part of page

---
+++ src/components/Footer.vue
@@ -0,0 +1,44 @@
+ <template>
+	<div class="footerClass">
+        <div class="footerImgClass">
+            animation
+		</div>
+        <div>
+            
+        </div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Footer',
+	mounted() {
+		//console.log(window.scrollY);
+	}
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* @import '../styles/howTo.css'; */
+
+@font-face {
+	font-family: Rubik;
+	src: url("../assets/fonts/Rubik-Regular.ttf");
+}
+
+.footerClass {
+    position: absolute;
+    width: 1508px;
+    height: 909.82px;
+    margin-left: -113px;
+    margin-top: 4447.18px;
+    mix-blend-mode: normal;
+    /* Background img */
+    background-image: url("../assets/footer/BG.png");
+	background-repeat: no-repeat;
+    background-position-x: 108px;
+    background-position-y: 170.39px;
+}
+</style>
